b46a1759fa2c357f3be81d201a6cc616768aead2
Lilith <76746408+LilithDPortes@users.noreply.github.com>
2021-09-27 21:58:16 -0300

    Add files via upload

---
+++ corredorjs.js
@@ -0,0 +1,21 @@
+const calcular = document.getElementById(`btnCalcular`);
+
+function CalcularCalorias() {
+    const nome = document.getElementById(`nome`).value;
+    const peso = document.getElementById(`peso`).value;
+    const distancia = document.getElementById(`distancia`).value;
+    const horas = document.getElementById(`horas`).value;
+    const minutos = document.getElementById(`minutos`).value;
+    const segundos = document.getElementById(`segundos`).value;
+    const resultado = document.getElementById(`pResultado`);
+
+    let horastotais = parseInt(horas) + parseInt(minutos / 60) + parseInt(segundos / 3600);
+    let velocidade = (parseFloat(distancia) / horastotais).toFixed(2);
+    let pace = (parseInt(horas * 60) + parseInt(minutos) / parseFloat(distancia)).toFixed(2);
+    let caloria = (velocidade * peso * 0.0175 * (parseInt(horas) * 60 + parseInt(minutos))).toFixed(3);
+
+    resultado.removeAttribute(`style`);
+    resultado.textContent = `Olá, ${nome}! Você percorreu ${distancia} km em ${horas}h, ${minutos} min e ${segundos}s. Isso quer dizer que a sua velocidade média foi de ${velocidade} km/h, o que o corresponde a um pace de ${pace} min/km. Com ${peso}kg, você gastou cerca de ${caloria} cal por minuto.`;
+}
+
+calcular.addEventListener(`click`, CalcularCalorias);
